33b624436c5a9528b58e1f8ac09064e8306f3730
yzc <rd_yzc@ely.work>
2020-04-15 14:37:47 +0800

    curentUser 信息写死

--- src/pages/user/login/index.jsx
+++ src/pages/user/login/index.jsx
@@ -37,7 +37,7 @@ const Login = props => {
 
           <UserName
             name="userName"
-            placeholder="用户名: admin or user"
+            placeholder="用户名: "
             rules={[
               {
                 required: true,
@@ -47,7 +47,7 @@ const Login = props => {
           />
           <Password
             name="password"
-            placeholder="密码: ant.design"
+            placeholder="密码: "
             rules={[
               {
                 required: true,
